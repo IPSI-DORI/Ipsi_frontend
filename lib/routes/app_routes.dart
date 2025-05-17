81c056a5100fbd6f0f7c011476905b70ce5d98b9
신혜서 <125953570+tlsgptj@users.noreply.github.com>
2025-05-17 13:10:09 +0900

    ✨ Chatbot build

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -4,11 +4,14 @@ import 'package:flutter/material.dart';
 import 'package:ipsi_frontend/features/home/screens/home_screen.dart';
 import 'package:ipsi_frontend/features/onboarding/screens/onboarding_screen.dart';
 import '../features/auth/screens/login_screen.dart';
+import "../features/chatbot/screens/chatbot_screens.dart";
 
 class AppRoutes {
   static const String auth = '/auth';
+  static const String chatbot = 'chatbot';
 
   static Map<String, WidgetBuilder> get routes => {
-    auth: (context) => HomeScreen()
+    auth: (context) => HomeScreen(),
+    chatbot: (context) => ChatbotScreen()
   };
 }
